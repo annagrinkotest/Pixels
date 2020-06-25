1d2ce3e5c146dc39028f3a3bffbef192b4b494ce
annagrinkotest <64903335+annagrinkotest@users.noreply.github.com>
2020-06-25 20:56:04 +0300

    add corrected file

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const widthCanvas = 14;
   let currentTool = document.querySelector(".tool-selected").id;
   let currentColor = document.querySelector(".color-selected").style.background;
-  
+
   document.querySelector(".colors").addEventListener("click", (e) => {
     document.querySelector(".color-selected").classList.remove("color-selected");
     e.target.classList.add("color-selected");
@@ -17,72 +17,54 @@ document.addEventListener('DOMContentLoaded', () => {
       currentTool = document.querySelector(".tool-selected").id;
     })
   })
-  pixels.forEach((element,i) => {
+  pixels.forEach((element, i) => {
     element.addEventListener("click", () => {
       if (currentTool === "tool-pencil") {
         element.style.background = currentColor;
-        element.setAttribute("id" , "pixel-selected");
       }
       else if (currentTool === "tool-fill") {
+        let colorPixel = element.style.background;
         pixels.forEach((element) => {
-          if (element.id !== "pixel-selected") {
-            element.style.background = currentColor; }
+          if (colorPixel === element.style.background) {
+            element.style.background = currentColor;
+          }
         })
       }
       else if (currentTool === "tool-erase") {
         element.style.background = "#eee";
-        element.setAttribute("id" , "pixel-selected");
       }
-      else  {
-          pixels[i].style.background = currentColor;
-          pixels[i].setAttribute("id" , "pixel-selected");
-          if (i > 0 && i < (widthCanvas - 1)) {
-            pixels[i + 1].style.background = currentColor;
-            pixels[i + 1].setAttribute("id" , "pixel-selected");
-            pixels[i - 1].style.background = currentColor;
-            pixels[i - 1].setAttribute("id" , "pixel-selected");
-            pixels[i + widthCanvas].style.background = currentColor;
-            pixels[i + widthCanvas].setAttribute("id" , "pixel-selected");
-          }
-          else if (i % widthCanvas === 0 && i !== 0) {
-            pixels[i + 1].style.background = currentColor;
-            pixels[i + 1].setAttribute("id" , "pixel-selected");
-            pixels[i - widthCanvas].style.background = currentColor;
-            pixels[i - widthCanvas].setAttribute("id" , "pixel-selected");
-            pixels[i + widthCanvas].style.background = currentColor;
-            pixels[i + widthCanvas].setAttribute("id" , "pixel-selected");
-          }
-          else if (i % widthCanvas === widthCanvas - 1 && i !== widthCanvas - 1) {
-            pixels[i - 1].style.background = currentColor;
-            pixels[i - 1].setAttribute("id" , "pixel-selected");
-            pixels[i - widthCanvas].style.background = currentColor;
-            pixels[i - widthCanvas].setAttribute("id" , "pixel-selected");
-            pixels[i + widthCanvas].style.background = currentColor;
-            pixels[i + widthCanvas].setAttribute("id" , "pixel-selected");
-          }
-          else if (i === 0) {
-            pixels[i + 1].style.background = currentColor;
-            pixels[i + 1].setAttribute("id" , "pixel-selected");
-            pixels[i + widthCanvas].style.background = currentColor;
-            pixels[i + widthCanvas].setAttribute("id" , "pixel-selected");
-          }
-          else if (i === widthCanvas - 1) {
-            pixels[i - 1].style.background = currentColor;
-            pixels[i - 1].setAttribute("id" , "pixel-selected");
-            pixels[i + widthCanvas].style.background = currentColor;
-            pixels[i + widthCanvas].setAttribute("id" , "pixel-selected");
-          }
-          else {
-            pixels[i + 1].style.background = currentColor;
-            pixels[i + 1].setAttribute("id" , "pixel-selected");
-            pixels[i - 1].style.background = currentColor;
-            pixels[i - 1].setAttribute("id" , "pixel-selected");
-            pixels[i - widthCanvas].style.background = currentColor;
-            pixels[i - widthCanvas].setAttribute("id" , "pixel-selected");
-            pixels[i + widthCanvas].style.background = currentColor;
-            pixels[i + widthCanvas].setAttribute("id" , "pixel-selected");
-          }
-        }      
-      })
+      else {
+        pixels[i].style.background = currentColor;
+        if (i > 0 && i < (widthCanvas - 1)) {
+          pixels[i + 1].style.background = currentColor;
+          pixels[i - 1].style.background = currentColor;
+          pixels[i + widthCanvas].style.background = currentColor;
+        }
+        else if (i % widthCanvas === 0 && i !== 0) {
+          pixels[i + 1].style.background = currentColor;
+          pixels[i - widthCanvas].style.background = currentColor;
+          pixels[i + widthCanvas].style.background = currentColor;
+        }
+        else if (i % widthCanvas === widthCanvas - 1 && i !== widthCanvas - 1) {
+          pixels[i - 1].style.background = currentColor;
+          pixels[i - widthCanvas].style.background = currentColor;
+          pixels[i + widthCanvas].style.background = currentColor;
+        }
+        else if (i === 0) {
+          pixels[i + 1].style.background = currentColor;
+          pixels[i + widthCanvas].style.background = currentColor;
+        }
+        else if (i === widthCanvas - 1) {
+          pixels[i - 1].style.background = currentColor;
+          pixels[i + widthCanvas].style.background = currentColor;
+        }
+        else {
+          pixels[i + 1].style.background = currentColor;
+          pixels[i - 1].style.background = currentColor;
+          pixels[i - widthCanvas].style.background = currentColor;
+          pixels[i + widthCanvas].style.background = currentColor;
+        }
+      }
     })
+  })
 });
