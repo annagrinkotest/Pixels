0fa2d5e35f0abb463760c9925b95fa8d167f94de
annagrinkotest <64903335+annagrinkotest@users.noreply.github.com>
2020-07-04 19:19:35 +0300

    Add file

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     e.target.classList.add("color-selected");
     currentColor = document.querySelector(".color-selected").style.background;
   })
+
   tools.forEach(element => {
     element.addEventListener("click", () => {
       document.querySelector(".tool-selected").classList.remove("tool-selected");
@@ -17,54 +18,73 @@ document.addEventListener('DOMContentLoaded', () => {
       currentTool = document.querySelector(".tool-selected").id;
     })
   })
+
   pixels.forEach((element, i) => {
     element.addEventListener("click", () => {
       if (currentTool === "tool-pencil") {
-        element.style.background = currentColor;
+        drawWithPencil (element);
       }
       else if (currentTool === "tool-fill") {
-        let colorPixel = element.style.background;
-        pixels.forEach((element) => {
-          if (colorPixel === element.style.background) {
-            element.style.background = currentColor;
-          }
-        })
+        drawWithFill (element);
       }
       else if (currentTool === "tool-erase") {
-        element.style.background = "#eee";
+        drawWithErase (element);
       }
-      else {
-        pixels[i].style.background = currentColor;
-        if (i > 0 && i < (widthCanvas - 1)) {
-          pixels[i + 1].style.background = currentColor;
-          pixels[i - 1].style.background = currentColor;
-          pixels[i + widthCanvas].style.background = currentColor;
-        }
-        else if (i % widthCanvas === 0 && i !== 0) {
-          pixels[i + 1].style.background = currentColor;
-          pixels[i - widthCanvas].style.background = currentColor;
-          pixels[i + widthCanvas].style.background = currentColor;
-        }
-        else if (i % widthCanvas === widthCanvas - 1 && i !== widthCanvas - 1) {
-          pixels[i - 1].style.background = currentColor;
-          pixels[i - widthCanvas].style.background = currentColor;
-          pixels[i + widthCanvas].style.background = currentColor;
-        }
-        else if (i === 0) {
-          pixels[i + 1].style.background = currentColor;
-          pixels[i + widthCanvas].style.background = currentColor;
-        }
-        else if (i === widthCanvas - 1) {
-          pixels[i - 1].style.background = currentColor;
-          pixels[i + widthCanvas].style.background = currentColor;
-        }
-        else {
-          pixels[i + 1].style.background = currentColor;
-          pixels[i - 1].style.background = currentColor;
-          pixels[i - widthCanvas].style.background = currentColor;
-          pixels[i + widthCanvas].style.background = currentColor;
-        }
+      else if (currentTool === "tool-brush") {
+        drawWithBrush (element,i);
       }
     })
   })
+
+  function drawWithPencil (element) {
+    element.style.background = currentColor;
+  }
+
+  function drawWithFill (element) {
+    let colorClickPixel =  element.style.background;
+    
+    pixels.forEach((element, i) => {
+     if (element.style.background === colorClickPixel) {
+      element.style.background = currentColor;
+      }  
+    })  
+  }
+
+  function drawWithErase (element) {
+    element.style.background = "#eee";
+  }
+
+  function drawWithBrush (element,i) {
+    pixels[i].style.background = currentColor;
+    if (i > 0 && i < (widthCanvas - 1)) {
+      pixels[i + 1].style.background = currentColor;
+      pixels[i - 1].style.background = currentColor;
+      pixels[i + widthCanvas].style.background = currentColor;
+    }
+    else if (i % widthCanvas === 0 && i !== 0) {
+      pixels[i + 1].style.background = currentColor;
+      pixels[i - widthCanvas].style.background = currentColor;
+      pixels[i + widthCanvas].style.background = currentColor;
+    }
+    else if (i % widthCanvas === widthCanvas - 1 && i !== widthCanvas - 1) {
+      pixels[i - 1].style.background = currentColor;
+      pixels[i - widthCanvas].style.background = currentColor;
+      pixels[i + widthCanvas].style.background = currentColor;
+    }
+    else if (i === 0) {
+      pixels[i + 1].style.background = currentColor;
+      pixels[i + widthCanvas].style.background = currentColor;
+    }
+    else if (i === widthCanvas - 1) {
+      pixels[i - 1].style.background = currentColor;
+      pixels[i + widthCanvas].style.background = currentColor;
+    }
+    else {
+      pixels[i + 1].style.background = currentColor;
+      pixels[i - 1].style.background = currentColor;
+      pixels[i - widthCanvas].style.background = currentColor;
+      pixels[i + widthCanvas].style.background = currentColor;
+    }
+  }
+  
 });
